efe5c2f4f93ce084157f52e0d3c4c195bfe0844c
Nicolas Pulido <npulido@zonehacks.net>
2022-10-07 18:48:57 -0700

    add Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+const tabs = [{
+    route: "/home",
+    icon: 'faHome',
+    label: "Home",
+  }, {
+    route: "/search",
+    icon: 'faSearch',
+    label: "Search",
+  },
+]
+
+export const Footer = (props) => {
+  return (
+    <>
+    </>
+  )
+}
