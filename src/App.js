1e0d92062824998ce7e233d56f0ed22ed582d690
Nicolas Pulido <npulido@zonehacks.net>
2022-10-07 18:48:32 -0700

    Using swiper

--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
-import { useState } from "react";
-import { List, ListGroup } from "react-bootstrap";
-import './App.css';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Card, Image } from "react-bootstrap";
+import "swiper/css";
+
+import { Slider } from "./components/Slider/Slider";
+import { Footer } from "./components/Footer/Footer";
+import "./App.css";
+import "./styles.css";
 
 function App() {
-  const myAr = Array.from({length:20}, (x,i)=>i);
-  const shuffle = (o) => {
-    for (var j, x, i = o.length;
-         i;
-         j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
-    );
-    return o;
-  }
-  const chunks = (ar) => {
-    let ar1 = [];
-    let count = ar.length / 4;
-    for (let i=0; i<count; i++) {
-      ar1.push(<ListGroup.Item key={i}>{ar.splice(0, 4)}</ListGroup.Item>);
-    }
-    console.log(ar1);
-    return ar1;
+  const media = "http://localhost:8002/v1/telegram/media";
+
+  const slide = (img, txt) => {
+    return (
+      <SwiperSlide>
+        <Card>
+          <Card.Header>
+            header
+          </Card.Header>
+        <Image
+            className="w-80"
+            src={img}
+            roundedCircle={true}
+            thumbnail={true}
+        />
+          <Card.Footer>
+            {txt}
+          </Card.Footer>
+        </Card>
+      </SwiperSlide>
+    )
   };
+
   return (
     <div className="App">
       <header className="App-header">
-        <ListGroup>
-          {chunks(shuffle(myAr))}
-        </ListGroup>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          className="mySwiper"
+        >
+          {slide(`${media}/photo_2022-09-08_08-03-15_7145625093855382528.jpg`, `eah they won't release the product to me, probably because it fails some surveillance checks, which annoys me greatly. You can use your real identity to buy it though. They just really want to know who you are, they seem to care more about that than selling product `)}
+          {slide(`${media}/photo_2022-08-17_16-54-04_7145625149136533504.jpg`)}
+          {slide(`https://api.p2pmarket.net/v1/telegram/@bitcoinp2pmarketplace?msg_id=1758&thumb=1`)}
+          {slide(`${media}/photo_2022-07-06_12-05-05_7143767263615310848.jpg`)}
+        </Swiper>
       </header>
+      <Footer>
+      </Footer>
     </div>
   );
 }
