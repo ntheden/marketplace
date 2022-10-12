2cf9297650f7a5d0029c9dc8441d4a178392a422
Nicolas Pulido <npulido@zonehacks.net>
2022-10-12 10:08:11 -0700

    refactor: move functions into Navigator
    in prep for using Buttons and removing reactstrap dependence,
    so that I can have better control of icon cluster

--- src/components/Navigator/Navigator.js
+++ src/components/Navigator/Navigator.js
@@ -1,7 +1,8 @@
 import { BsGearFill } from 'react-icons/bs';
 import { FaLayerGroup, FaBitcoin, FaHome, FaBookmark } from 'react-icons/fa';
+import { Button } from 'react-bootstrap';
 import { Nav, NavItem } from 'reactstrap';
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 
 import Filter from "../Filter/Filter";
 import Arrangement from "../Arrangement/Arrangement";
@@ -9,7 +10,10 @@ import Search from "../Search/Search";
 import DarkMode from "../DarkMode/DarkMode";
 import styles from "./Navigator.module.scss";
 
-const RightCluster = () => {
+
+const TopRightCluster = () => {
+  // This is outside of Navigator component because Filter
+  // and Arrangement (and later Search) cause refreshes in Feed
   const pathname = useLocation().pathname
   return (
       <div className={styles.iconCluster}>
@@ -28,47 +32,46 @@ const RightCluster = () => {
   );
 };
 
-const TopNavBar = (
-  <nav className="navbar navbar-custom navbar-expand-md fixed-top bg-dark" role="navigation">
-    <div className="container-fluid">
-      <div>
-        <FaBitcoin className={styles.bitcoin} />
-        <a className="navbar-brand text-" href="/">Marketplace</a>
-      </div>
-      <RightCluster />
-    </div>
-  </nav>
-);
-
-
-const navItem = (pathname, icon) => {
-  return (
-    <NavItem>
-      <NavLink to={pathname} className="nav-link">
-        <div className="row d-flex flex-column justify-content-center align-items-center">
-          {icon}
-          { /* Only show label in larger viewports <div>{tab.label}</div> */ }
-        </div>
-      </NavLink>
-    </NavItem>
-  );
-};
-
-
-const BottomNavBar = (
-  <nav className="navbar navbar-custom fixed-bottom bg-dark" role="navigation">
-    <Nav className="w-100">
-      <div className="d-flex flex-row justify-content-around w-100">
-        {navItem("/bookmarks", <FaBookmark className={styles.icon} />)}
-        {navItem("/", <FaHome className={styles.icon} />)}
-        {navItem("/settings", <BsGearFill className={styles.icon} />)}
-      </div>
-    </Nav>
-  </nav>
-);
 
 
 const Navigator = () => {
+  const navItem = (pathname, icon) => {
+    return (
+      <NavItem>
+        <NavLink to={pathname} className="nav-link">
+          <div className="row d-flex flex-column justify-content-center align-items-center">
+            {icon}
+            { /* Only show label in larger viewports <div>{tab.label}</div> */ }
+          </div>
+        </NavLink>
+      </NavItem>
+    );
+  };
+
+  const TopNavBar = (
+    <nav className="navbar navbar-custom navbar-expand-md fixed-top bg-dark" role="navigation">
+      <div className="container-fluid">
+        <div>
+          <FaBitcoin className={styles.bitcoin} />
+          <a className="navbar-brand text-" href="/">Marketplace</a>
+        </div>
+        <TopRightCluster />
+      </div>
+    </nav>
+  );
+
+  const BottomNavBar = (
+    <nav className="navbar navbar-custom fixed-bottom bg-dark" role="navigation">
+      <Nav className="w-100">
+        <div className="d-flex flex-row justify-content-around w-100">
+          {navItem("/bookmarks", <FaBookmark className={styles.icon} />)}
+          {navItem("/", <FaHome className={styles.icon} />)}
+          {navItem("/settings", <BsGearFill className={styles.icon} />)}
+        </div>
+      </Nav>
+    </nav>
+  );
+
   return (
     <div>
       { TopNavBar }
