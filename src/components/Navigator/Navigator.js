916f4b6957f9a0cfde2d617d2a383ca484456cd0
Nicolas Pulido <npulido@zonehacks.net>
2022-10-11 22:18:43 -0700

    wip

--- src/components/Navigator/Navigator.js
+++ src/components/Navigator/Navigator.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGear } from '@fortawesome/free-solid-svg-icons';
 import { FaLayerGroup, FaBitcoin, FaHome, FaBookmark } from 'react-icons/fa';
@@ -11,73 +12,83 @@ import Search from "../Search/Search";
 import DarkMode from "../DarkMode/DarkMode";
 import styles from "./Navigator.module.scss";
 
-const RightCluster = () => {
-  const pathname = useLocation().pathname
-  return (
-      <div className={styles.iconCluster}>
-        {(pathname === "/bookmarks" || pathname === "/") ? (
-          <>
-           <Filter />
-           <Arrangement />
-          </>
-         ) : (
-          <>
-          </>
-         )}
-        <Search />
-        <DarkMode />
-      </div>
-  );
-};
-
-const TopNavBar = (
-  <nav className="navbar navbar-expand-md fixed-top bg-dark" role="navigation">
-    <div className="container-fluid">
-      <div>
-        <FaBitcoin className={styles.bitcoin} />
-        <a className="navbar-brand text-" href="/">Marketplace</a>
-      </div>
-      <RightCluster />
-    </div>
-  </nav>
-);
-
-
-const navItem = (pathname, icon) => {
-  return (
-    <NavItem>
-      <NavLink to={pathname} className="nav-link">
-        <div className="row d-flex flex-column justify-content-center align-items-center">
-          {icon}
-          { /* Only show label in larger viewports <div>{tab.label}</div> */ }
-        </div>
-      </NavLink>
-    </NavItem>
-  );
-};
-
-
-const BottomNavBar = (
-  <nav className="navbar fixed-bottom bg-dark" role="navigation">
-    <Nav className="w-100">
-      <div className="d-flex flex-row justify-content-around w-100">
-        {navItem("/bookmarks", <FaBookmark className={styles.icon} />)}
-        {navItem("/", <FaHome className={styles.icon} />)}
-        {navItem(
-          "/settings",
-          <FontAwesomeIcon size="lg" icon={faGear} className={styles.icon}/>
-        )}
-      </div>
-    </Nav>
-  </nav>
-);
-
 
 const Navigator = () => {
+  const [homeIcon, setHomeIcon] = useState();
+
+  const navItem = (pathname, icon) => {
+    let newIcon = {...icon};
+    const onClick = (e, pathname) => {
+      newIcon.className = styles.iconRed;
+      setHomeIcon(newIcon);
+      console.log('clicked', pathname, "icon", newIcon);
+    };
+    return (
+      <NavItem onClick={(e) => onClick(e, pathname)}>
+        <NavLink to={pathname} className="nav-link">
+          <div className="row d-flex flex-column justify-content-center align-items-center">
+            {icon}
+            { /* Only show label in larger viewports <div>{tab.label}</div> */ }
+          </div>
+        </NavLink>
+      </NavItem>
+    );
+  };
+
+
+  const RightCluster = () => {
+    const pathname = useLocation().pathname
+    return (
+        <div className={styles.iconCluster}>
+          {(pathname === "/bookmarks" || pathname === "/") ? (
+            <>
+             <Filter />
+             <Arrangement />
+            </>
+           ) : (
+            <>
+            </>
+           )}
+          <Search />
+          <DarkMode />
+        </div>
+    );
+  };
+
+  const TopNavBar = (
+    <nav className="navbar navbar-expand-md fixed-top bg-dark" role="navigation">
+      <div className="container-fluid">
+        <div>
+          <FaBitcoin className={styles.bitcoin} />
+          <a className="navbar-brand text-" href="/">Marketplace</a>
+        </div>
+        <RightCluster />
+      </div>
+    </nav>
+  );
+
+
+  const BottomNavBar = () => {
+    return(
+      <nav className="navbar fixed-bottom bg-dark" role="navigation">
+        <Nav className="w-100">
+          <div className="d-flex flex-row justify-content-around w-100">
+            {navItem("/bookmarks", <FaBookmark className={styles.icon} />)}
+            {navItem("/", <FaHome className={styles.icon} />)}
+            {navItem(
+              "/settings",
+              <FontAwesomeIcon size="lg" icon={faGear} className={styles.icon} />
+            )}
+          </div>
+        </Nav>
+      </nav>
+    );
+  };
+
   return (
     <div>
       { TopNavBar }
-      { BottomNavBar }
+      <BottomNavBar />
     </div>
   )
 };
